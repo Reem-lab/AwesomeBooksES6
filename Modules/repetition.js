b107220da27092e0e5b2a14dca8367043032848f
Reem <janinareem@gmail.com>
2022-01-11 00:29:57 +0200

    Fix linters errors

--- Modules/repetition.js
+++ Modules/repetition.js
@@ -1,38 +1,28 @@
-import { Book } from "./book.js";
+// eslint-disable-next-line import/no-cycle
+import { Book } from './book.js';
 
-const msg = document.querySelector("#errorMsg")
-const deleteBtn = document.createElement("button")
-// const inpAuthor = document.querySelector("#author");
-// const inpTitle = document.querySelector("#title");
+const msg = document.querySelector('#errorMsg');
+const deleteBtn = document.createElement('button');
 
-export const checkRepetition = book => {
+// eslint-disable-next-line import/prefer-default-export
+export const checkRepetition = (book) => {
   for (let i = 0; i < Book.collection.length; i += 1) {
-    const currentBook = Book.collection[i]
+    const currentBook = Book.collection[i];
     if (
-      currentBook.title.toLowerCase() === book.title.toLowerCase() &&
-      currentBook.author.toLowerCase() === book.author.toLowerCase()
+      currentBook.title.toLowerCase() === book.title.toLowerCase()
+      && currentBook.author.toLowerCase() === book.author.toLowerCase()
     ) {
-      msg.textContent = " ❗ This Book is Already in the list"
-      deleteBtn.textContent = "X"
-      deleteBtn.classList.add("close-modal")
-      msg.appendChild(deleteBtn)
+      msg.textContent = ' ❗ This Book is Already in the list';
+      deleteBtn.textContent = 'X';
+      deleteBtn.classList.add('close-modal');
+      msg.appendChild(deleteBtn);
 
-      return false
+      return false;
     }
   }
-  return true
-}
+  return true;
+};
 
-deleteBtn.addEventListener("click", () => {
-  msg.classList.add("hidden")
-})
-
-// export const addBookToLibrary = () => {
-//   const inputTitleValue = inpTitle.value
-//   const inputAuthorValue = inpAuthor.value
-//   const book = new Book(inputTitleValue, inputAuthorValue)
-//   if (checkRepetition(book)) {
-//     book.saveBooks()
-//   }
-// }
-
+deleteBtn.addEventListener('click', () => {
+  msg.classList.add('hidden');
+});
--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-import {Book} from "./Modules/book.js";
-import { addBookToLibrary } from "./Modules/book.js"
-import { addNavLinks } from "./Modules/navlinks.js"
-const inpAuthor = document.querySelector("#author");
-const inpTitle = document.querySelector("#title");
+import { Book, addBookToLibrary } from './Modules/book.js';
+
+import { addNavLinks } from './Modules/navlinks.js';
+import { DateTime } from './node_modules/luxon/src/luxon.js';
+
+const inpAuthor = document.querySelector('#author');
+const inpTitle = document.querySelector('#title');
 const dateContainer = document.querySelector('.date');
-import { DateTime } from "./node_modules/luxon/src/luxon.js";
 
-const theTime = DateTime.now().toLocaleString(DateTime.DATETIME_MED)
-dateContainer.innerHTML = `${theTime}`
+const theTime = DateTime.now().toLocaleString(DateTime.DATETIME_MED);
+dateContainer.innerHTML = `${theTime}`;
 
-const book = new Book()
-const form = document.querySelector(".form")
-form.addEventListener("submit", (e) => {
-  e.preventDefault()
-  addBookToLibrary()
-  inpTitle.value = ""
-  inpAuthor.value = ""
-})
+const book = new Book();
+const form = document.querySelector('.form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  addBookToLibrary();
+  inpTitle.value = '';
+  inpAuthor.value = '';
+});
 
-document.addEventListener("DOMContentLoaded", () => {
-  if (localStorage.getItem("books") !== null) {
-    const myBooks = JSON.parse(localStorage.getItem("books"))
-    Book.collection = myBooks
+document.addEventListener('DOMContentLoaded', () => {
+  if (localStorage.getItem('books') !== null) {
+    const myBooks = JSON.parse(localStorage.getItem('books'));
+    Book.collection = myBooks;
   }
-  book.iterateBooks()
-})
+  book.iterateBooks();
+});
 
 addNavLinks();
-
