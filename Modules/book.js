900fefbb4d7611d7780d34514c0a088f6743e77a
Reem <janinareem@gmail.com>
2022-01-10 19:03:33 +0200

    Add package.json file

---
+++ Modules/book.js
@@ -0,0 +1,59 @@
+const bookContainer = document.querySelector(".book-container");
+const inpTitle = document.querySelector("#title");
+const inpAutor = document.querySelector("#author");
+
+class Book {
+    static collection = []
+  
+    constructor(title, author) {
+      this.title = title
+      this.author = author
+    }
+  
+    static setLocalStorage = () => {
+      const data = JSON.stringify(Book.collection)
+      localStorage.setItem("books", data)
+    }
+  
+    deleteBook = (title, author) => {
+      Book.collection = Book.collection.filter(
+        (book) => book.title !== title || book.author !== author
+      )
+    }
+  
+    showBook(book) {
+      const bookDiv = document.createElement("DIV")
+      const bookTitle = document.createElement("p")
+      const deleteBtn = document.createElement("BUTTON")
+      deleteBtn.classList.add("btn-remove")
+  
+      const title = `"${book.title}" by ${book.author}`
+      bookTitle.textContent = title
+      deleteBtn.textContent = "Remove"
+  
+      bookDiv.appendChild(bookTitle)
+      bookDiv.appendChild(deleteBtn)
+      bookContainer.appendChild(bookDiv)
+      deleteBtn.addEventListener("click", () => {
+        this.deleteBook(book.title, book.author)
+        Book.setLocalStorage(Book.collection)
+        bookContainer.removeChild(bookDiv)
+      })
+    }
+  
+    iterateBooks() {
+      Book.collection.forEach((book) => {
+        this.showBook(book)
+      })
+    }
+  
+    saveBooks() {
+      const book = new Book(inpTitle.value, inpAutor.value)
+      Book.collection.push(book)
+      Book.setLocalStorage(Book.collection)
+      this.showBook(book)
+    }
+  }
+
+
+ export class Book {...};
